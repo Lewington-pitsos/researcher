cdf24d858fe3915bab9d89f891fc7a4c87a4e573
Louka Ewington-Pitsos <lewingtonpitsos@gmail.com>
2020-08-21 07:26:47 +1000

    change paradigm from having researcher run experiments to simply having it record parameters and experiment results once the experiment has finished

--- researcher/run.py
+++ researcher/run.py
@@ -19,14 +19,10 @@ def reduced_params(params, unwanted_keys):
 
     return {k: params[k] for k in params.keys() - unwanted_keys}
 
-def run_experiment(params, experiment_fn, save_path):
+
+def record_experiment(params, results, save_path):
     cloned_params = copy.deepcopy(params)
     param_hash = get_hash(cloned_params)
-
-    print("Commencing experiment {}\n".format(param_hash))
-
-    # experiment_fn must return a Results instance.
-    results = experiment_fn(reduced_params(cloned_params, ['title', 'notes']), identifier=param_hash)
 
     cloned_params["hash"] = param_hash
     cloned_params["timestamp"] = datetime.datetime.now().strftime(DATE_FORMAT)
@@ -37,8 +33,4 @@ def run_experiment(params, experiment_fn, save_path):
         title = "no_title"
 
     save_experiment(save_path, "{}_{}".format(title, param_hash), parameters=cloned_params, results=results.view())
-    
-    print("Finished experiment {}\n".format(param_hash))
-
-    return results
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="researcher",
-    version="0.0.11",
+    version="0.0.14",
     author="Louka Ewington-Pitsos",
     author_email="lewingtonpitsos@gmail.com",
     description="A tool for recording the results of data science experiments",
@@ -18,5 +18,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=['scipy', 'numpy', 'matplotlib'],
     python_requires='>=3.6',
 )
